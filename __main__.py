3da1949a936bbe719e9982cc60df70a6a9d235e8
madeindjs <madeindjs@gmail.com>
2016-07-21 20:12:04 +0200

    try to connect on all wifi

--- __main__.py
+++ __main__.py
@@ -1,7 +1,13 @@
-from wifi import Cell, Scheme
+from wifi import Cell, Scheme, exceptions
 
 if __name__ == '__main__':
 
-    # need `sudo` to show all networks
-    for cell in Cell.all('wlan0'):
-      print(cell)
+	# need `sudo` to show all networks
+	for cell in Cell.all('wlan0'):
+		
+		try:
+			scheme = Scheme.for_cell('wlan0', 'home', cell, 'test')
+			scheme.activate()
+		except exceptions.ConnectionError as e:
+			print("Can't connect to {} with `{}` passkey".format(cell, 'test'))
+		
