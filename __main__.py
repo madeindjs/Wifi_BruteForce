83a2b72a823e80b03e94f46203554f2988ba326a
madeindjs <madeindjs@gmail.com>
2016-08-21 21:32:52 +0200

    add ips scanner

--- __main__.py
+++ __main__.py
@@ -3,15 +3,20 @@
 import argparse
 
 import wifi_bruteforce
+import network_scanner
 
 def main():
 
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-w", "--wifi_brute_force", action="store_true", help="Try to brute force all wifi detected by this device")
+	parser.add_argument("-a", "--scan_ips", action="store_true", help="Scan all IPs on this networks")
 	args = parser.parse_args()
 
 	if args.wifi_brute_force:
 		wifi_bruteforce.start()
+
+	if args.scan_ips:
+		network_scanner.scan()
 
 
 
---
+++ network_scanner.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# inspired by http://null-byte.wonderhowto.com/how-to/build-arp-scanner-using-scapy-and-python-0162731/
+
+import sys
+from datetime import datetime
+
+from scapy.all import srp,Ether,ARP,conf
+
+def scan_ips(interface='wlan0', ips='192.168.1.0/24'):
+	"""a simple ARP scan with Scapy"""
+	try:
+		print('[*] Start to scan')
+		conf.verb = 0 # hide all verbose of scapy
+		ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+		arp = ARP(pdst = ips)
+		answer, unanswered = srp(ether/arp, timeout = 2, iface = interface, inter = 0.1)
+
+		for sent, received in answer:
+			print(received.summary())
+
+	except KeyboardInterupt:
+		print('[*] User requested Shutdown')
+		print('[*] Quitting...')
+		sys.exit(1)
